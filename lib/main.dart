095b91fc56511d3663b0b28577fa393690462566
QSHti <dinmukhammed.altynbek@eek.ee>
2024-01-13 00:41:34 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase/firebase_options.dart';
 import 'screen/home_screen.dart';
 
-Future <void>  main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 
